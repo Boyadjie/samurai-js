748468ff0120e6e0ff5b22ce3ffcb8fd5f1e7bc0
emeric boyadjian <emericboy73@gmail.com>
2022-01-28 14:52:36 +0100

    generate random elements as tree, rock or floor

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
+$(document).ready(function(){
   // ---------------------
   // Functions
-
-  function createArena(container) {
+  
+  function createArena() {
     let arena = "";
     for (let i = 0; i < 9; i++) {
       let row = `<div class="row">`;
@@ -11,13 +12,50 @@
       row += `</div>`;
       arena += row;
     }
-    container.html(arena);
+    $('#gameArea').html(arena);
   }
+  
+  function atributeRandomClass() {
+    let cells = $('.cell');
+  
+    // Generate 3 trees
+    for (let i = 0; i < 3; i++) {
+      let randIndex = Math.floor((Math.random() * (cells.length - 1)));
+      let treeCell = cells.eq(randIndex);
+      if(treeCell.hasClass("tree") == false && treeCell.hasClass("rock") == false) {
+        treeCell.addClass("tree");
+      }else {
+        console.log(treeCell.classList);
+      }
+    }
 
+    // Generate 2 rocks
+    for (let j = 0; j < 2; j++) {
+      let randIndex = Math.floor((Math.random() * (cells.length - 1)));
+      let rockCell = cells.eq(randIndex);
+      if(rockCell.hasClass("tree") == false && rockCell.hasClass("rock") == false) {
+        rockCell.addClass("rock");
+      }else {
+        console.log(rockCell.classList);
+      }
+    }
+
+    // Generate ground cell where they not contain any other class than cell
+    for (let k = 0; k < cells.length; k++) {
+      const cell = cells.eq(k);
+      if(cell.hasClass("tree") == false && cell.hasClass("rock") == false) {
+        cell.addClass("floor");
+      }
+    }
+    
+    // console.log(cells);
+  }
+  
   // End Functions
   // ---------------------
+  
+  createArena();
+  atributeRandomClass();
 
-
-  let arenaContainer = $('#gameArea');
-  console.log(arenaContainer);
-  createArena(arenaContainer);
+});
+  
