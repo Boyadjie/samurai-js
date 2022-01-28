343088e0875fe9fb0fe4bb1b84bc2fa8e51582d0
emeric boyadjian <emericboy73@gmail.com>
2022-01-28 13:08:49 +0100

    add assets (not fixed) and generate game area

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('coucou');
+  // ---------------------
+  // Functions
+
+  function createArena(container) {
+    let arena = "";
+    for (let i = 0; i < 9; i++) {
+      let row = `<div class="row">`;
+      for (let j = 0; j < 9; j++) {
+        row += `<div class="cell"></div>`;
+      }
+      row += `</div>`;
+      arena += row;
+    }
+    container.html(arena);
+  }
+
+  // End Functions
+  // ---------------------
+
+
+  let arenaContainer = $('#gameArea');
+  console.log(arenaContainer);
+  createArena(arenaContainer);
